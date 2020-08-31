d705c7afdc6e63f4537584fe65b526cff133ee91
Kamali Means <darenkmeans@gmail.com>
2020-08-31 11:47:32 -0700

    Create v1 models

---
+++ models/comic.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const writerSchema = new Schema ({
+    name: String
+});
+
+const artistSchema = new Schema ({
+    name: String
+});
+
+const comicSchema = new Schema({
+    title: String,
+    writers: [writerSchema],
+    artists: [artistSchema],
+    isOwned: {
+        type: Boolean,
+        default: false
+    },
+    isRead: {
+        type: Boolean,
+        default: false
+    }
+});
+
+module.exports = mongoose.model('Comic', comicSchema);
--- models/user.js
+++ models/user.js
@@ -3,7 +3,12 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
     name: String,
-    googleId: String
+    googleId: String,
+    //This doesn't have to be a list/array, but I'm just gonna try to get it working then maybe experiment later..
+    weave: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Comic'
+    }]
 });
 
 module.exports = mongoose.model('User', userSchema);
