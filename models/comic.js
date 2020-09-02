90f893a2846a82697e99dc3c79116e6ab785688c
Kamali Means <darenkmeans@gmail.com>
2020-09-02 12:03:39 -0700

    Change Select to Datalist on NEW comic

--- models/comic.js
+++ models/comic.js
@@ -5,10 +5,12 @@ const comicSchema = new Schema({
     title: String,
     issueNum: Number,
     writers: [{
-        type: Schema.Types.ObjectId, ref: 'Writer'
+        type: Schema.Types.ObjectId, ref: 'Writer',
+        default: null
     }],
     artists: [{
-        type: Schema.Types.ObjectId, ref: 'Artist'
+        type: Schema.Types.ObjectId, ref: 'Artist',
+        default: null
     }],
     isOwned: {
         type: Boolean,
